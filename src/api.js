f13fc02b64fe56249bb001d4fd5245fe5878e2bf
Soham Ghugare <soham.ghugare@gmail.com>
2023-11-16 20:19:17 +0530

    feat: integrated nurses routes

--- src/api.js
+++ src/api.js
@@ -4,12 +4,15 @@ const app = express();
 import cors from 'cors';
 import doctorsRouter from './routes/doctors.js';
 import patientsRouter from './routes/patients.js';
+import nursesRouter from './routes/nurses.js';
+
 
 app.use(json());
 app.use(cors());
 
 app.use('/doctors', doctorsRouter);
 app.use('/patients', patientsRouter);
+app.use('/nurses', nursesRouter);
 
 const PORT = process.env.PORT || 8080;
 
